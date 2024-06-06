b1635fb726e9af13c4b7d6f4532a192712fe1f7b
divyanshu tailor <128120812+divyyuanshutailor@users.noreply.github.com>
2024-06-06 19:06:48 +0530

    counter java script

---
+++ design websites ui/lobi.ai/javscript/rockpaperscissorgame/rps_game.js
@@ -0,0 +1,8 @@
+const playerDivyanshu = document.getElementById("player");
+const computerRandomer = document.getElementById("computer");
+const resultGame = document.getElementById("result");
+const rockBtn = document.getElementById("rockbtn");
+const paperBtn = document.getElementById("paperbtn");
+const scissiorBtn = document.getElementById("Scissiorbtn");
+
+
