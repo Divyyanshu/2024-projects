e8d558290ae2ff544a3d6c902ca8f70dc5f62bce
divyanshu tailor <128120812+divyyuanshutailor@users.noreply.github.com>
2024-06-06 18:50:53 +0530

    counter java script

---
+++ design websites ui/lobi.ai/javscript/lesson1js/index.js
@@ -0,0 +1,17 @@
+const count = document.getElementById("count");
+const add = document.getElementById("addbtn");
+const subtract = document.getElementById("subbtn");
+
+let counter = 0;
+
+function adds (){
+    counter = counter + 1;
+    count.innerText = `Counter Value is : ${counter}`
+}
+function subs (){
+    counter = counter - 1;
+    count.innerText = `Counter Value is : ${counter}`
+}
+
+add.addEventListener("click", adds);
+subtract.addEventListener("click", subs);
