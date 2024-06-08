e0403f9d3e72b8729fc2e0e7747cace2f10f2657
divyanshu tailor <128120812+divyyuanshutailor@users.noreply.github.com>
2024-06-08 13:45:39 +0530

    js code rps game completed

--- design websites ui/lobi.ai/javscript/rockpaperscissorgame/rps_game.js
+++ design websites ui/lobi.ai/javscript/rockpaperscissorgame/rps_game.js
@@ -4,37 +4,110 @@ const resultGame = document.getElementById("result");
 const rockBtn = document.getElementById("rockbtn");
 const paperBtn = document.getElementById("paperbtn");
 const scissiorBtn = document.getElementById("Scissiorbtn");
-const randomBtn = document.getElementById('randomBtn');
-const randomNumber = document.getElementById('randomnumber');
+// const randomBtn = document.getElementById('randomBtn');
+// const randomNumber = document.getElementById('randomnumber');
+const drawPoint = document.getElementById("draw");
+const playerPoint = document.getElementById('playerPoint');
+const computerPoint = document.getElementById('computerPoint');
+
 
 let playerChoice = "";
 let computerChoice = "";
-
+let gameResult = "";
+let draw = "";
+let playerWins = "";
+let computerWins = "";
 
 // build a function for random choice
-// function computerChoose(){
-//     let randomchoice = Math.floor(Math.random()*3)+1;
-//     alert(randomchoice);
-// }
+function computerChoose() {
+      let randomchoice = Math.floor(Math.random() * 3) + 1;
+
+      switch (randomchoice) {
+            case 1:
+                  computerChoice = "Rock"
+                  break;
+            case 2:
+                  computerChoice = "Paper"
+                  break;
+            case 3:
+                  computerChoice = "Scissior"
+                  break;
+      }
+         computerRandomer.innerText = `Computer : ${computerChoice}`
+}
 // button functioning
 
+// check result fumction
 
-rockBtn.addEventListener('click',()=>{
+function checkResult(){
+     if(playerChoice === computerChoice){
+      gameResult = "Draw !"
+     }
+     else if (playerChoice === "Rock" && computerChoice === "Paper"){
+       gameResult = "Computer Win"
+     }
+     else if (playerChoice === "Paper" && computerChoice === "Rock"){
+       gameResult = "Player Win"
+     }
+     else if (playerChoice === "Scissior" && computerChoice === "Paper"){
+       gameResult = "Player Win"
+     }
+     else if (playerChoice === "Paper" && computerChoice === "Scissior"){
+       gameResult = "Computer Win"
+     }
+     else if (playerChoice === "Rock" && computerChoice === "Scissior"){
+       gameResult = "Player Win"
+     }
+     else if (playerChoice === "Paper" && computerChoice === "Rock"){
+       gameResult = "Computer Win"
+     }
+     else if(playerChoice === "Scissior" && computerChoice === "Rock"){
+      gameResult = "Computer Win"
+     }
+     else{
+      gameResult = "Error Result not found";
+     }
+
+     resultGame.innerText = `Game Result : ${gameResult}`
+   
+        if(gameResult === "Draw !"){
+            draw++;
+        }else if (gameResult === "Player Win" ){
+            playerWins++;
+        }else if (gameResult === "Computer Win" ){
+            computerWins++;
+        }
+        else{
+            alert('result points error')
+        }
+   
+        drawPoint.innerText = `Draw points :${draw}`;
+        playerPoint.innerText = `Player points :${playerWins}`;
+        computerPoint.innerText = `Computer points :${computerWins}`
+}
+
+rockBtn.addEventListener('click', () => {
       playerChoice = "Rock";
       playerDivyanshu.innerText = `Player : ${playerChoice}`;
+      computerChoose();
+      checkResult();
 })
-scissiorBtn.addEventListener('click',()=>{
+scissiorBtn.addEventListener('click', () => {
       playerChoice = "Scissior";
       playerDivyanshu.innerText = `Player : ${playerChoice}`;
+      computerChoose();
+      checkResult();
 })
-paperBtn.addEventListener('click',()=>{
+paperBtn.addEventListener('click', () => {
       playerChoice = "Paper";
       playerDivyanshu.innerText = `Player : ${playerChoice}`;
+      computerChoose();
+      checkResult();
 })
 
-randomBtn.addEventListener('click',()=>{
-    let randomchoice = Math.floor(Math.random()*3)+1;
-    computerChoice = randomchoice;
-    randomNumber.innerText = `Genrated Computer Number : ${computerChoice}`
-    // alert(randomchoice);
-})
+// randomBtn.addEventListener('click', () => {
+//       let randomchoice = Math.floor(Math.random() * 3) + 1;
+//       computerChoice = randomchoice;
+//       randomNumber.innerText = `Genrated Computer Number : ${computerChoice}`
+//       // alert(randomchoice);
+// })
