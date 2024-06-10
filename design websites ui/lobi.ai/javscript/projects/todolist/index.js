7515ae03049324fbb40b99096004661c68895662
divyanshu tailor <128120812+divyyuanshutailor@users.noreply.github.com>
2024-06-10 18:50:29 +0530

    project to do completed

---
+++ design websites ui/lobi.ai/javscript/projects/todolist/index.js
@@ -0,0 +1,27 @@
+const inputTask = document.getElementById("input-task");
+const addTaskbtn = document.getElementById("addTaskbtn");
+const taskListElement = document.getElementById("taskItem");
+const deleteTaskbtn =document.getElementById("deleteTaskbtn")
+
+let taskList = [];
+
+// renderlist
+function randerList (){
+    taskListElement.innerHTML = "";
+    for(let i = 0; i <= taskList.length-1; i++){
+    const newTask =document.createElement('li');
+    newTask.innerText = taskList[i]
+    taskListElement.appendChild(newTask);}
+}
+
+function addTask (){
+    taskList.push(inputTask.value);
+   randerList();
+}
+
+function deleteTask (){
+    taskList.pop(inputTask.value);
+   randerList();
+}
+addTaskbtn.addEventListener('click',addTask);
+deleteTaskbtn.addEventListener('click',deleteTask);
