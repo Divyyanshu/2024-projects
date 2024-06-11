b05a0e8360f5874dd954f54eb573a53eb8960503
divyanshu tailor <128120812+divyyuanshutailor@users.noreply.github.com>
2024-06-11 23:07:40 +0530

    add promises

---
+++ design websites ui/lobi.ai/javscript/promises/index.js
@@ -0,0 +1,24 @@
+
+// promises (resolve,reject)
+ let walking = function(){
+     console.log("i am walking")
+ }
+ let sleeping = function(){
+     console.log("i am sleppping")
+ }
+ let running = function(){
+     console.log("i am running")
+ }
+ let talking = function(){
+     console.log("i am talking")
+ }
+ let eating = function(){
+     console.log("i am eating")
+ }
+
+
+//  setTimeout(walking , 2000)
+//  setTimeout(sleeping ,4000)
+//  setInterval(running ,2000)
+//  setTimeout(talking ,8000)
+//  setTimeout(eating ,10000)
