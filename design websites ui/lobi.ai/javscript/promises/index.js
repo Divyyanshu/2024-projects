841e8e63e51178754070e9756af430559ea3a128
divyanshu tailor <128120812+divyyuanshutailor@users.noreply.github.com>
2024-06-11 23:19:24 +0530

    add promises

--- design websites ui/lobi.ai/javscript/promises/index.js
+++ design websites ui/lobi.ai/javscript/promises/index.js
@@ -1,20 +1,31 @@
 
 // promises (resolve,reject)
  let walking = function(){
-     console.log("i am walking")
+    return new Promise(()=>{
+        let walkER = TRUE ;
+        if(walkER === res){
+            console.log("helloo reslover")
+        }
+        else{
+            console.log("hello rejector")
+        }
+    })
  }
- let sleeping = function(){
-     console.log("i am sleppping")
- }
- let running = function(){
-     console.log("i am running")
- }
- let talking = function(){
-     console.log("i am talking")
- }
- let eating = function(){
-     console.log("i am eating")
- }
+
+
+ walking().then((div)=>console.log(div)).catch((jak)=>console.error(jak))
+//  let sleeping = function(){
+//      console.log("i am sleppping")
+//  }
+//  let running = function(){
+//      console.log("i am running")
+//  }
+//  let talking = function(){
+//      console.log("i am talking")
+//  }
+//  let eating = function(){
+//      console.log("i am eating")
+//  }
 
 
 //  setTimeout(walking , 2000)
