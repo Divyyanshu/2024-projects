d7aac430201d6a0cf0a9c16f77af8bdd9698aefe
divyanshu tailor <128120812+divyyuanshutailor@users.noreply.github.com>
2024-06-15 00:30:09 +0530

    loop replace by map

---
+++ design websites ui/lobi.ai/javscript/projects/weatherApp/waether.js
@@ -0,0 +1,11 @@
+const heading = document.getElementById("heading")
+const longitudeInput = document.getElementById("longitudeInput")
+const latitudeInput = document.getElementById("latitudeInput")
+const SubmitInputBtn = document.getElementById("inputBtn")
+const latitude = document.getElementById("latitude")
+const longitude = document.getElementById("longitude")
+const temprature = document.getElementById("temprature")
+const windSpeed = document.getElementById("windSpeed")
+const rain = document.getElementById("rain")
+const dayAurNight = document.getElementById("dayAurNight")
+
